96158c5ec05f9b1dd5fcd4bffe447b0ade85c709
Sudhansu Bhushan Mishra <sbmishra2000@gmail.com>
2020-02-15 21:34:17 +0530

    Add index.html

--- js/linear-search-sketch.js
+++ js/linear-search-sketch.js
@@ -4,8 +4,10 @@ function setup() {
   for (let i = 1; i <= 10; i++) {
     values.push(Math.floor(random()*101))
   }
-  createCanvas(640,480);
-  document.write('<h1>'+values+'</h1>');
+  // createCanvas(640,480);
+  // let h1 = document.createElement('h1');
+  // h1.innerText = values.join(', ');
+  // document.body.querySelector('main').appendChild(h1);
 }
 
 function draw() {
